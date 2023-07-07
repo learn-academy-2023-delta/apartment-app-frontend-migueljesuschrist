2efc2138e0eda7ecd26102f26ffd868240336ca6
Chris Frey <87246187+Chris-Frey@users.noreply.github.com>
2023-07-07 14:41:53 +0000

    removed extra code

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,42 +1,25 @@
 import React from 'react'
-// import styles from '../styles/Footer.css'
-
-import { Navbar, Button, Link, Text, Card, Radio } from "@nextui-org/react";
+import styles from '../styles/Footer.css'
+import { Navbar } from "@nextui-org/react";
 import { FooterLayout } from "./FooterLayout.js";
-import { NavLink } from 'react-router-dom';
 
 
-const Header = () => {
+
+const Footer = () => {
 
   const [variant, setVariant] = React.useState("static");
 
   const variants = ["static", "floating", "sticky"];
+  
   return (
-    // <div>Header</div>
     <>
     <FooterLayout>
       <Navbar isBordered variant={variant}>
-        {/* <Navbar.Brand> */}
-          {/* <AcmeLogo /> */}
-          {/* <Text b color="inherit" hideIn="xs">
-            ACME
-          </Text> */}
-        {/* </Navbar.Brand> */}
         <Navbar.Content hideIn="xs">
           <Navbar.Link href="/">Veteran Home Finder</Navbar.Link>
-          {/* <Navbar.Link isActive href="#">Customers</Navbar.Link> */}
-          {/* <Navbar.Link href="/apartmentindex/">Listings</Navbar.Link> */}
           <Navbar.Link href="#">Miguel | Jesus | Chris</Navbar.Link>
         </Navbar.Content>
         <Navbar.Content>
-          {/* <Navbar.Link color="inherit" href="#">
-            Sign In
-          </Navbar.Link> */}
-          {/* <Navbar.Item>
-            <Button auto flat as={Link} href="#">
-              Sign Up
-            </Button>
-          </Navbar.Item> */}
         </Navbar.Content>
       </Navbar>
       
@@ -47,6 +30,6 @@ const Header = () => {
 
 
 
-export default Header
+export default Footer
 
     
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,6 @@
 import React from 'react'
 import styles from '../styles/Header.css'
-// import {
-//   Collapse,
-//   Navbar,
-//   NavbarToggler,
-//   NavbarBrand,
-//   Nav,
-//   NavItem,
-//   NavLink,
-//   NavbarText,
-// } from 'reactstrap';
-import { Navbar, Button, Link, Text, Card, Radio } from "@nextui-org/react";
+import { Navbar, Button, Link} from "@nextui-org/react";
 import { HeaderLayout } from "./HeaderLayout.js";
 import { NavLink } from 'react-router-dom';
 
@@ -20,41 +10,14 @@ const Header = () => {
   const [variant, setVariant] = React.useState("static");
 
   const variants = ["static", "floating", "sticky"];
+
   return (
-    // <div>Header</div>
     <>
-    <div>
-      {/* <Navbar className='nav-bar'>
-        <NavbarBrand href="/">reactstrap</NavbarBrand>
-        <NavbarToggler onClick={toggle} />
-        <Collapse isOpen={isOpen} navbar>
-          <Nav className="me-auto" navbar>
-            <NavItem>
-              <NavLink href="/components/">Components</NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink href="https://github.com/reactstrap/reactstrap">
-                GitHub
-              </NavLink>
-            </NavItem>
-          </Nav>
-          <NavbarText>Simple Text</NavbarText>
-        </Collapse>
-      </Navbar> */}
-    </div>
     <HeaderLayout>
       <Navbar isBordered variant={variant}>
-        {/* <Navbar.Brand> */}
-          {/* <AcmeLogo /> */}
-          {/* <Text b color="inherit" hideIn="xs">
-            ACME
-          </Text> */}
-        {/* </Navbar.Brand> */}
         <Navbar.Content hideIn="xs">
           <Navbar.Link href="/">Home</Navbar.Link>
-          {/* <Navbar.Link isActive href="#">Customers</Navbar.Link> */}
           <Navbar.Link href="/apartmentindex/">Listings</Navbar.Link>
-          {/* <Navbar.Link href="#">Company</Navbar.Link> */}
         </Navbar.Content>
         <Navbar.Content>
           <Navbar.Link color="inherit" href="#">
@@ -67,7 +30,6 @@ const Header = () => {
           </Navbar.Item>
         </Navbar.Content>
       </Navbar>
-      
     </HeaderLayout>
     </>
   );
